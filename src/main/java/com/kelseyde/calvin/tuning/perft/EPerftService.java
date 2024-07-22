1ea7bb51f201e45c23f2733eda5d3e5d3c162d11
kelseyde <daniel.kelsey@hotmail.co.uk>
2024-07-23 01:10:56 +0200

    Try comparing eval between nnue and hce

--- src/main/java/com/kelseyde/calvin/tuning/perft/EPerftService.java
+++ src/main/java/com/kelseyde/calvin/tuning/perft/EPerftService.java
@@ -4,7 +4,9 @@ import com.kelseyde.calvin.board.Board;
 import com.kelseyde.calvin.board.Move;
 import com.kelseyde.calvin.engine.EngineInitializer;
 import com.kelseyde.calvin.evaluation.Evaluator;
+import com.kelseyde.calvin.evaluation.NNUE;
 import com.kelseyde.calvin.generation.MoveGenerator;
+import com.kelseyde.calvin.utils.notation.FEN;
 
 import java.util.List;
 
@@ -12,10 +14,12 @@ public class EPerftService {
 
     private final MoveGenerator moveGenerator;
     private final Evaluator evaluator;
+    private final NNUE nnue;
 
     public EPerftService(Board board) {
         moveGenerator = new MoveGenerator();
         evaluator = new Evaluator(EngineInitializer.loadDefaultConfig());
+        nnue = new NNUE(board);
         evaluator.evaluate(board);
     }
 
@@ -25,9 +29,16 @@ public class EPerftService {
         }
         List<Move> moves = moveGenerator.generateMoves(board);
         for (Move move : moves) {
+            nnue.makeMove(board, move);
             board.makeMove(move);
-            evaluator.evaluate(board);
+            int eval = evaluator.evaluate(board);
+            int nnueEval = nnue.evaluate(board);
+            if (Math.signum(eval) != Math.signum(nnueEval) && Math.abs(eval - nnueEval) > 600) {
+//            if (board.isWhiteToMove()) {
+                System.out.println("NNUE eval: " + nnueEval + " vs. eval: " + eval + "  " + FEN.toFEN(board));
+            }
             ePerft(board, depth - 1);
+            nnue.unmakeMove();
             board.unmakeMove();
         }
     }
